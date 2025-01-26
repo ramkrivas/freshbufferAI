66cff6bd8edfae87e7a00cc248ade0d693d4abc0
ramkrivas <ramkrivas@gmail.com>
2025-01-26 19:29:47 +0100

    swagger api document project setup

---
+++ apps/api-documentation/src/configs/swagger.config.ts
@@ -0,0 +1,44 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const swaggerUiOptions = {
+    failOnErrors: true, // Throw when parsing errors
+    baseDir: __dirname, // Base directory which we use to locate your JSDOC files
+    exposeApiDocs: true,
+    definition: {
+        openapi: '3.0.3',
+        info: {
+            title: 'FreshbufferAI APIs',
+            summary: 'Interactive swagger-ui auto-generated API docs from express, based on a swagger.yml file',
+            version: '1.0.0',
+            description:
+                'This module serves auto-generated swagger-ui generated API docs from FreshbufferAI express backend, based on a swagger.yml file. Swagger is available on: http://localhost:6655/api-docs',
+            license: {
+                name: 'Apache 2.0',
+                url: 'https://github.com/FlowiseAI/Flowise/blob/main/LICENSE.md'
+            },
+            contact: {
+                name: 'FreshbufferAI',
+                email: 'support@freshbuffer.nl'
+            }
+        },
+        servers: [
+            {
+                url: 'http://localhost:3000/api/v1',
+                description: 'FreshbufferAI App Services'
+            }
+        ]
+    },
+    apis: [`${process.cwd()}/dist/routes/**/*.js`, `${process.cwd()}/src/yml/swagger.yml`]
+}
+
+// https://github.com/swagger-api/swagger-ui/blob/master/docs/usage/configuration.md
+const swaggerExplorerOptions = {
+    swaggerOptions: {
+        validatorUrl: '127.0.0.1'
+    },
+    explorer: true
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerUiOptions)
+
+export { swaggerDocs, swaggerExplorerOptions }
