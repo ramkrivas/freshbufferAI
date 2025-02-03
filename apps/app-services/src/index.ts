5374c4b4a79b1ed0d60326b712d6e2fafd8a06d9
ramkrivas <ramkrivas@gmail.com>
2025-02-03 21:05:19 +0100

    initial drafts

---
+++ apps/app-services/src/index.ts
@@ -0,0 +1,161 @@
+import cors from 'cors'
+import express, { Request, Response } from 'express'
+import 'global-agent/bootstrap'
+import http from 'http'
+import path from 'path'
+import { Server } from 'socket.io'
+import { DataSource } from 'typeorm'
+import { getDataSource } from './DataSource'
+import errorHandlerMiddleware from './middlewares/errors'
+import freshbufferaiApiV1Router from './routes'
+import { getNodeModulesPackagePath } from './utils'
+import { getAllowedIframeOrigins, getCorsOptions, sanitizeMiddleware } from './utils/XSS'
+import logger, { expressRequestLogger } from './utils/logger'
+
+declare global {
+    namespace Express {
+        interface Request {
+            io?: Server
+        }
+        namespace Multer {
+            interface File {
+                bucket: string
+                key: string
+                acl: string
+                contentType: string
+                contentDisposition: null
+                storageClass: string
+                serverSideEncryption: null
+                metadata: any
+                location: string
+                etag: string
+            }
+        }
+    }
+}
+
+export class App {
+    app: express.Application
+
+    AppDataSource: DataSource = getDataSource()
+
+    constructor() {
+        this.app = express()
+    }
+
+    async initDatabase() {
+        // Initialize database
+        try {
+            await this.AppDataSource.initialize()
+            logger.info('📦 [app-services]: Data Source is initializing...')
+
+            // Run Migrations Scripts
+            await this.AppDataSource.runMigrations({ transaction: 'each' })
+        } catch (error) {
+            logger.error('❌ [app-services]: Error during Data Source initialization:', error)
+        }
+    }
+
+    async config(socketIO?: Server) {
+        // Limit is needed to allow sending/receiving base64 encoded string
+        const freshbufferai_file_size_limit = process.env.FRESHBUFFERAI_FILE_SIZE_LIMIT || '50mb'
+        this.app.use(express.json({ limit: freshbufferai_file_size_limit }))
+        this.app.use(express.urlencoded({ limit: freshbufferai_file_size_limit, extended: true }))
+        if (process.env.NUMBER_OF_PROXIES && parseInt(process.env.NUMBER_OF_PROXIES) > 0)
+            this.app.set('trust proxy', parseInt(process.env.NUMBER_OF_PROXIES))
+
+        // Allow access from specified domains
+        this.app.use(cors(getCorsOptions()))
+
+        // Allow embedding from specified domains.
+        this.app.use((req, res, next) => {
+            const allowedOrigins = getAllowedIframeOrigins()
+            if (allowedOrigins == '*') {
+                next()
+            } else {
+                const csp = `frame-ancestors ${allowedOrigins}`
+                res.setHeader('Content-Security-Policy', csp)
+                next()
+            }
+        })
+
+        // Switch off the default 'X-Powered-By: Express' header
+        this.app.disable('x-powered-by')
+
+        // Add the expressRequestLogger middleware to log all requests
+        this.app.use(expressRequestLogger)
+
+        // Add the sanitizeMiddleware to guard against XSS
+        this.app.use(sanitizeMiddleware)
+
+        // Make io accessible to our router on req.io
+        this.app.use((req, res, next) => {
+            req.io = socketIO
+            next()
+        })
+
+        this.app.use('/api/v1', freshbufferaiApiV1Router)
+
+        // ----------------------------------------
+        // Configure number of proxies in Host Environment
+        // ----------------------------------------
+        this.app.get('/api/v1/ip', (request, response) => {
+            response.send({
+                ip: request.ip,
+                msg: 'Check returned IP address in the response. If it matches your current IP address ( which you can get by going to http://ip.nfriedly.com/ or https://api.ipify.org/ ), then the number of proxies is correct and the rate limiter should now work correctly. If not, increase the number of proxies by 1 and restart Cloud-Hosted FreshbufferAI until the IP address matches your own. Visit https://docs.freshbufferai.com/configuration/rate-limit#cloud-hosted-rate-limit-setup-guide for more information.'
+            })
+        })
+
+        // ----------------------------------------
+        // Serve UI static
+        // ----------------------------------------
+
+        const packagePath = getNodeModulesPackagePath('freshbufferai-ui')
+        const uiBuildPath = path.join(packagePath, 'build')
+        const uiHtmlPath = path.join(packagePath, 'build', 'index.html')
+
+        this.app.use('/', express.static(uiBuildPath))
+
+        // All other requests not handled will return React app
+        this.app.use((req: Request, res: Response) => {
+            res.sendFile(uiHtmlPath)
+        })
+
+        // Error handling
+        this.app.use(errorHandlerMiddleware)
+    }
+
+    async stopApp() {
+        try {
+            const removePromises: any[] = []
+            await Promise.all(removePromises)
+        } catch (e) {
+            logger.error(`❌[app-services]: FreshbufferAI app-services shut down error: ${e}`)
+        }
+    }
+}
+
+let serverApp: App | undefined
+
+export async function start(): Promise<void> {
+    serverApp = new App()
+
+    const host = process.env.HOST
+    const port = parseInt(process.env.PORT || '', 10) || 3000
+    const server = http.createServer(serverApp.app)
+
+    const io = new Server(server, {
+        cors: getCorsOptions()
+    })
+
+    await serverApp.initDatabase()
+    await serverApp.config(io)
+
+    server.listen(port, host, () => {
+        logger.info(`⚡️ [app-services]: FreshbufferAI app-services is listening at ${host ? 'http://' + host : ''}:${port}`)
+    })
+}
+
+export function getInstance(): App | undefined {
+    return serverApp
+}
